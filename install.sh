fc970e1303b50f3cf402b51fb1a9bb48157e4711
Samy Lerari <samy.lerari@gmail.com>
2020-10-01 16:26:40 +0200

    Installation script, new versions of starbases modules and new editct

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+rm -fr /c/users/samyl/Documents/Paradox\ Interactive/Stellaris/mod/SLMod/*
+
+cp -R common/ /c/users/samyl/Documents/Paradox\ Interactive/Stellaris/mod/SLMod/common/
+cp -R gfx/ /c/users/samyl/Documents/Paradox\ Interactive/Stellaris/mod/SLMod/gfx/
+cp -R interface/ /c/users/samyl/Documents/Paradox\ Interactive/Stellaris/mod/SLMod/interface/
+cp -R localisation/ /c/users/samyl/Documents/Paradox\ Interactive/Stellaris/mod/SLMod/localisation/
